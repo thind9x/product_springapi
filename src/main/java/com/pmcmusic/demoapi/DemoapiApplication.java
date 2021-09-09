53eae4df5fc574682946d32fe69b5cc5d346ce1d
phuongmychi <thind9x@gmail.com>
2021-09-09 20:46:07 +0700

    s

--- src/main/java/com/pmcmusic/demoapi/DemoapiApplication.java
+++ src/main/java/com/pmcmusic/demoapi/DemoapiApplication.java
@@ -2,25 +2,20 @@ package com.pmcmusic.demoapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableWebSecurity
 @SpringBootApplication(scanBasePackages = { "com.pmcmusic.demoapi" })
-
 public class DemoapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoapiApplication.class, args);
 	}
 
-	public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	// public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-		@Override
-		protected void configure(HttpSecurity security) throws Exception {
-			security.httpBasic().disable();
-		}
-	}
+	// 	@Override
+	// 	protected void configure(HttpSecurity security) throws Exception {
+	// 		security.httpBasic().disable();
+	// 	}
+	// }
 
 }
